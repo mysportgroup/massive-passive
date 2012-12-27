fb9582e0abc857a32594b0e0ddf6db8750acdac0
Robin Wittler <r.wittler@mysportgroup.de>
2012-12-27 11:36:08 +0100

    fix license header

--- mplib/daemon.py
+++ mplib/daemon.py
@@ -3,7 +3,7 @@
 
 __author__ = 'Robin Wittler'
 __contact__ = 'r.wittler@mysportgroup.de'
-__license__ = 'GPL3'
+__license__ = 'GPL3+'
 __version__ = '0.0.1'
 __copyright__ = '(c) 2012 by mysportgroup.de'
 
