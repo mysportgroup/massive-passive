2cb863445a7c9a89a31386d89c03e2b399b466b6
Robin Wittler <r.wittler@mysportgroup.de>
2013-05-10 19:11:42 +0200

    add fix for changing dict while iterate the same time

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from distutils.core import setup
 
 setup(
         name='massive-passive',
-        version='0.3.4',
+        version='0.3.5',
         description='A scheduler for passive nagios/icinga checks. This includes a server and client.',
         author='Robin Wittler',
         author_email='r.wittler@mysportgroup.de',
